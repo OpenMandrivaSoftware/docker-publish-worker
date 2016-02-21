12c704f7d1de3d1d3a3c19bea639fe7cd91c070f
Alexander Khryukin <alexander@mezon.ru>
2016-02-21 21:02:01 +0300

    drop armv7l

--- scripts/mdv/build.sh
+++ scripts/mdv/build.sh
@@ -58,9 +58,9 @@ if [ "$testing" == 'true' ] ; then
 fi
 
 # Checks that 'repository' directory exist
-mkdir -p $repository_path/{SRPMS,i586,x86_64,armv7l,armv7hl,aarch64}/$rep_name/$status/media_info
+mkdir -p $repository_path/{SRPMS,i586,x86_64,armv7hl,aarch64}/$rep_name/$status/media_info
 if [ "$use_debug_repo" == 'true' ] ; then
-  mkdir -p $repository_path/{SRPMS,i586,x86_64,armv7l,armv7hl,aarch64}/debug_$rep_name/$status/media_info
+  mkdir -p $repository_path/{SRPMS,i586,x86_64,armv7hl,aarch64}/debug_$rep_name/$status/media_info
 fi
 
 sign_rpm=0
@@ -183,7 +183,7 @@ function build_repo {
 }
 
 rx=0
-arches="SRPMS i586 x86_64 armv7l armv7hl aarch64"
+arches="SRPMS i586 x86_64 armv7hl aarch64"
 
 # Checks sync status of repository
 rep_locked=0
